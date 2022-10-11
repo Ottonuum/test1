102ff3e5cc2753d5dde64e5ec63887e750667ee7
ottonuum <Nuumotto1@gmail.com>
2022-10-11 13:58:07 +0300

    first commit

---
+++ hello.py
@@ -0,0 +1,11 @@
+# user_name = input('Mis on sinu nimi: ')
+
+# print('Hello, ' + user_name + '!')
+
+a = input('a: ')
+b = input('b: ')
+
+print(int(a) + int(b))
+print(int(a) - int(b))
+print(int(a) * int(b))
+print(int(a) / int(b))
---
+++ yl2.py
@@ -0,0 +1,11 @@
+import math
+
+raadius = int(input('Sisesta raadius:'))
+
+Ümbermõõt = 2 * math.pi * raadius
+
+pindala = math.pi * raadius ** 2
+
+print(Ümbermõõt)
+
+print(pindala)

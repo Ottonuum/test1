d94acf511bfeea1b5af9904b2d75621ee303982c
ottonuum <Nuumotto1@gmail.com>
2022-11-10 11:35:57 +0200

    commit

--- yl12.py
+++ yl12.py
@@ -1,2 +1,29 @@
 fruits = ['õun', 'pirn', 'banaan']
- 
+
+print('fruits')
+
+print(fruits[0])
+
+fruits.append('ploom')
+print(fruits[-1])
+
+fruits[2] = 'apelsiń'
+
+
+if 'õun' in [el.lower() for el in fruits]:
+    print('jah, õun on olemas')
+
+print(len(fruits))
+
+print(fruits[len(fruits)-1])
+
+del fruits[1]
+print(fruits)
+
+fruits.reverse()
+print(fruits)
+
+fruits.sort()
+print fruits()
+
+print(fruits[1:])

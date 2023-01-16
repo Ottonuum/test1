e1479167f91131f9ab4177c9b6a6987aafacfbe2
Ottonuum <Nuumotto1@gmail.com>
2023-01-16 14:13:27 +0200

    commit

---
+++ yl25.py
@@ -0,0 +1,15 @@
+me = {
+    'first_name': 'Otto',
+    'last_name': 'Nuum',
+    'birth_year': 2005,
+    'place_of_living': 'Upa',
+    'dessert': 'Kirju koer'
+}
+
+# print(me.get('place_of_living'))
+# print(me['place_of_living'])
+
+me['dessert'] = 'ice cream'
+
+for k, v in me.items():
+    print(k, v)

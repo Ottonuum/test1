9c2c6c496d07e2156e833949b6690c2391ba3b8a
Ottonuum <Nuumotto1@gmail.com>
2023-01-22 21:44:23 +0200

    commit

--- yl26.py
+++ yl26.py
@@ -1,9 +1,18 @@
-Sales_results = {
-    "Johnver":{
-        "Vanson":{
-            "Vamsey"{
-                "Mundyke"
-            }
-        }
-    }
+import pandas as pd
+
+revenue {
+    'Johnver': [190,325,682,829],
+    'Vanston': [140,19,14,140],
+    'Danbree': [1926,293,852,609],
+    'Vansey': [14,1491,56,120],
+    'Mundyke': [143,162,659,87]
 }
+
+expenses {
+    'Johnver': [120,300,50,67],
+    'Vanston': [65,10,299,254],
+    'Danbree': [890,23,1290,89],
+    'Vansey': [54,802,12,129],
+    'Mundyke': [430,235,145,76]
+}
+

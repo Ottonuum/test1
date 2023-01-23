b767d7d81782ea805399f80f82403864208b0aca
Ottonuum <Nuumotto1@gmail.com>
2023-01-23 09:11:26 +0200

    commit

--- yl26.py
+++ yl26.py
@@ -1,18 +1,20 @@
-import pandas as pd
+data = '''Revenue
 
-revenue {
-    'Johnver': [190,325,682,829],
-    'Vanston': [140,19,14,140],
-    'Danbree': [1926,293,852,609],
-    'Vansey': [14,1491,56,120],
-    'Mundyke': [143,162,659,87]
-}
+            Johnver Vanston Danbree Vansey  Mundyke
+Tea             190     140    1926     14      143
+Coffee          325      19     293   1491      162
+Water           682      14     852     56      659
+Milk            829     140     609    120       87
 
-expenses {
-    'Johnver': [120,300,50,67],
-    'Vanston': [65,10,299,254],
-    'Danbree': [890,23,1290,89],
-    'Vansey': [54,802,12,129],
-    'Mundyke': [430,235,145,76]
-}
+Expenses
+
+            Johnver Vanston Danbree Vansey  Mundyke
+Tea             120      65     890     54      430
+Coffee          300      10      23    802      235
+Water            50     299    1290     12      145
+Milk             67     254      89    129       76'''
+
+data_f = [line.split() for line in data.split('\n') if line]
+
+
 

e53ce736df34fbf9fc4ef18ab36b72e888ec8089
Ottonuum <Nuumotto1@gmail.com>
2023-03-13 18:20:08 +0200

    commit

--- yl23.py
+++ yl23.py
@@ -5,28 +5,28 @@ used_cards = []
 
 
 while (next_suit + next_rank) in used_cards:
-next_suit = random.choice(suits)
-next_rank = random.choice(ranks)
+    next_suit = random.choice(suits)
+    next_rank = random.choice(ranks)
 
 used_cards.append(next_suit + next_rank)
 
 return[next_suit, next_rank]
 def get_hand_score(hand):
     score=0
-    for card in hand 
+    for card in hand:
 
 
-def 
-    next_suit = random.choice(suits)
-    next_rank = random.choice(ranks)
+    def:
+        next_suit = random.choice(suits)
+        next_rank = random.choice(ranks)
 
-    while next_rank
-    print(next_rank) 
-    next_suit = random.choice
-    next_rank = random
+        while next_rank
+        print(next_rank) 
+        next_suit = random.choice
+        next_rank = random
 
-for i in range(2):
-    palyer_hand.append(choose_card())
-    dealer_hand.append(choose_card())
+    for i in range(2):
+        palyer_hand.append(choose_card())
+        dealer_hand.append(choose_card())
 
-    print(player_hand, get_hand_score)
+        print(player_hand, get_hand_score)
